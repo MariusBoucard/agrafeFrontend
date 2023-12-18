5d9c4ed88f3cc38492f5e161a86362073578f48c
Marius Boucard Bocciarelli <marius.boucard--bocciarelli@insa-rennes.fr>
2023-12-18 20:19:16 +0100

    added focale

--- src/views/FocaleView.vue
+++ src/views/FocaleView.vue
@@ -1,119 +1,122 @@
 <template>
     <div class="baseView">
-       <div class="header"> 
+        <div class="header">
             <p class="titleHeader">Focale</p>
-       </div>
+        </div>
 
-       <div class="centerDiv">
-        <div class="horizontalChooser">
+        <div class="centerDiv">
+            <div class="horizontalChooser">
 
-                <div v-for="focale in this.focales" :key="focale.id" :class="{ 'selected': focale.id === selectedFocal.id, 'tata': focale.id !== selectedFocal.id }" @click="changeSelectedFocale(focale.id)" class="rond">
+                <div v-for="focale in this.focales" :key="focale.id" :class="{ 'selected': focale.id === selectedFocal.id }"
+                    @click="changeSelectedFocale(focale.id)" class="rond">
                     {{ focale.numero }}
                 </div>
-        </div>
-
-        <div class="focalInfo">
-            <h1>{{ selectedFocal.titre }}</h1>
-            {{ focales }}
-            <p>Titre de l'oeuvre</p>
-            <p>technique</p>
-        </div>
-        {{ pdfUrl }}
-        <div style="display: flex;"> 
-            <div class="column" style="width: 10%;" @click="lastFocale()">
-            
-            <div class="middleIcon">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6">
-                    <path fill-rule="evenodd" d="M19.5 5.653c0-1.426-1.529-2.33-2.779-1.643l-11.54 6.348c-1.295.712-1.295 2.573 0 3.285l11.54 6.348c1.25.687 2.779-.217 2.779-1.643V5.653z" clip-rule="evenodd" />
-                </svg>
             </div>
-            <!-- Content for the first column -->
-        </div>
-        <div class="column" style="width: 80%;">
-            <!-- Content for the second column -->
-            <div style="width=50%">
-                <pdf :src="`${baseUrl}/save/saveFocale/${selectedFocal.id}/1.pdf`"></pdf>
+
+            <div class="focalInfo">
+                <h1 class="artiste">{{ selectedFocal.auteur }}</h1>
+                <p class="oeuvre">{{ selectedFocal.titre }}</p>
+                <p class="technique">{{ selectedFocal.technique }}</p>
+            </div>
+            <div style="display: flex;">
+                <div class="column" style="width: 10%;" @click="lastFocale()">
+
+                    <div class="middleIcon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6">
+                            <path fill-rule="evenodd"
+                                d="M19.5 5.653c0-1.426-1.529-2.33-2.779-1.643l-11.54 6.348c-1.295.712-1.295 2.573 0 3.285l11.54 6.348c1.25.687 2.779-.217 2.779-1.643V5.653z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <!-- Content for the first column -->
                 </div>
-                <div style="width=50%">
-                    <pdf :src="`${baseUrl}/save/saveFocale/${selectedFocal.id}/1.pdf`"></pdf>
+                <div class="column" style="width: 80%;">
+                    <!-- Content for the second column -->
+                    <div style="width:50%">
+                        <pdf :src="`${baseUrl}/save/saveFocale/${selectedFocal.id}/1.pdf`"></pdf>
+                    </div>
+                    <div style="width:50%">
+                        <pdf :src="`${baseUrl}/save/saveFocale/${selectedFocal.id}/1.pdf`"></pdf>
+
+                    </div>
 
                 </div>
+                <div class="column" style="width: 10%;">
+                    <div class="middleIcon" @click="nextFocale()">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6">
+                            <path fill-rule="evenodd"
+                                d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+                                clip-rule="evenodd" />
+                        </svg>
 
-        </div>
-        <div class="column" style="width: 10%;">
-            <div class="middleIcon" @click="nextFocale()">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-6 h-6">
-                <path fill-rule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clip-rule="evenodd" />
-                </svg>
+                    </div>
+                    <!-- Content for the third column -->
+                </div>
+
 
             </div>
-            <!-- Content for the third column -->
-        </div>
-       
-            
-        </div>
 
-       </div>
+        </div>
     </div>
-
 </template>
 
 <script>
 import pdf from 'vue3-pdf';
 import baseUrl from '../config';
 import axiosInstance from '@/axios';
-export default{
+export default {
     name: "FocaleView",
     components: {
         pdf
     },
     data() {
         return {
-            baseUrl : baseUrl,
-            pdfUrl: baseUrl+'/save/saveArticle/pdf/4vAAnGg4w3pSlRmOaqn1O.pdf', // Replace with the path to your PDF file
-            selectedFocal : { titre : "scooby doo"},
-            focales : []
+            baseUrl: baseUrl,
+            selectedFocal: { titre: "scooby doo", auteur: "scooby doo", technique: "scooby doo", id : 0 },
+            focales: []
         };
     },
     mounted() {
         this.getFocale()
+        this.focales.sort((a, b) => (a.numero > b.numero) ? 1 : -1)
+        this.selectedFocal = this.focales[this.focales.length - 1]
     },
     methods: {
-    changeSelectedFocale(id){
-        this.selectedFocal = this.focales.find(focale => focale.id == id)
-    },
-    
-        getFocale(){
-
-       axiosInstance
-      .get("/api/getPublicFocale")
-      .then((response) => {
-        this.focales = response.data;
-        console.log(this.focales)
-        this.focales.sort((a, b) => (a.numero > b.numero) ? 1 : -1)
-        this.selectedFocal = this.focales[this.focales.length-1]
-        //this.rubriques.push({id: 'Articles', rubrique: 'Articles'})
-      })
-      .catch((error) =>
-        this.$message({
-          message: error,
-          type: "error",
-          customClass: "custom-el-message",
-          duration: 1000,
-        })
-      );
-           
+        changeSelectedFocale(id) {
+            this.selectedFocal = this.focales.find(focale => focale.id == id)
         },
-        lastFocale(){
-                console.log(this.focales)
-                const index = this.focales.findIndex(focale => focale.id === this.selectedFocal.id);
-                if (index > 0) {
-                    this.selectedFocal = this.focales[index - 1];
-                } else {
-                    return this.selectedFocal;
-                }
-            },
-        nextFocale(){
+
+        getFocale() {
+
+            axiosInstance
+                .get("/api/getPublicFocale")
+                .then((response) => {
+                    this.focales = response.data;
+                    console.log(this.focales)
+                    this.focales.sort((a, b) => (a.numero > b.numero) ? 1 : -1)
+                    this.selectedFocal = this.focales[this.focales.length - 1]
+                    //this.rubriques.push({id: 'Articles', rubrique: 'Articles'})
+                })
+                .catch((error) =>
+                    this.$message({
+                        message: error,
+                        type: "error",
+                        customClass: "custom-el-message",
+                        duration: 1000,
+                    })
+                );
+
+        },
+        lastFocale() {
+            console.log(this.focales)
+            const index = this.focales.findIndex(focale => focale.id === this.selectedFocal.id);
+            if (index > 0) {
+                this.selectedFocal = this.focales[index - 1];
+            } else {
+                return this.selectedFocal;
+            }
+        },
+        nextFocale() {
             console.log(this.focales)
             const index = this.focales.findIndex(focale => focale.id === this.selectedFocal.id);
             if (index < this.focales.length - 1) {
@@ -126,50 +129,76 @@ export default{
 }
 </script>
 <style scoped>
-
-.horizontalChooser{
+.horizontalChooser {
     width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
 }
-.rond{
+.artiste{
+font-size: normal;
+font-weight: 30;
+letter-spacing: 2px; /* Adjust the value to change the space between letters */
+
+}
+.oeuvre{
+    font-family: 'bahnschrift', sans-serif;
+    margin-bottom: 8px;
+    font-style: italic;    
+}
+.technique{
+    margin-top: 0px;
+    font-family: 'bahnschrift', sans-serif;
+
+}
+.rond {
     border-radius: 50%;
     background-color: white;
     height: 30px;
-    width : 30px;
-    color : black;
+    width: 30px;
+    color: black;
     margin: 10px;
+    text-align: center;
+    justify-content: center;
+    display: flex;
+    align-items: center;
 }
-.rond:hover{
+
+.rond:hover {
     background-color: black;
-    color : white;
+    color: white;
 }
-.focalInfo{
-   padding-top: 5px;
+
+.focalInfo {
+    padding-top: 5px;
 }
-.baseView{
-    width:90%;
+
+.baseView {
+    width: 90%;
     margin: auto;
 }
-.column{
+
+.column {
     display: flex;
     width: 100%;
     justify-content: center;
     min-height: 70vh;
+    margin-bottom: 20px;;
 }
 
-.middleIcon{
+.middleIcon {
     width: 60px;
     height: 60px;
     margin-top: auto;
     margin-bottom: auto;
 }
-.header{
+
+.header {
     background-color: black;
-    color : white
+    color: white
 }
-.titleHeader{
+
+.titleHeader {
     font-size: x-large;
     font-family: 'Agrafe', sans-serif;
     width: 90%;
@@ -177,15 +206,16 @@ export default{
     margin-left: auto;
 }
 
-.centerDiv{
-    color : white;
+.centerDiv {
+    color: white;
     background-color: black;
     margin-top: 20px;
-    margin-bottom: 60px;;
+    margin-bottom: 60px;
+    ;
 }
-.selected{
+
+.selected {
     background-color: black;
-    color : white;
+    color: white;
     border: 1px solid white;
-}
-</style>
+}</style>
